48f016a1003408457b3bb9a2bfdbb888de2f3daf
RollerCoasterRoss <rsw3020@gmail.com>
2020-01-15 10:28:50 -0600

    Added 4 cars to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# product = Product.new(name: "Ferrari Enzo", price: 1000000, image_url: "https://www.sportscarmarket.com/wp-content/uploads/2018/10/2003-ferrari-enzo-front.jpg", description: "An extremely rare supercar from Ferrari.")
+# product.save
+# product = Product.new(name: "Nissan Skyline", price: 125000, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/2012_Nissan_GT-R_Egoist.jpg/560px-2012_Nissan_GT-R_Egoist.jpg", description: "A legendary Japanese supercar.")
+# product.save
+# product = Product.new(name: "Mitsubishi Lancer Evo 8 MR", price: 80000, image_url: "https://media.fastestlaps.com/mitsubishi-lancer-evolution-viii-mr.jpg?640x350m", description: "A really fast go-kart.")
+# product.save
+# product = Product.new(name: "Aston Martin V12 Vanquish", price: 250000, image_url: "https://media.ed.edmunds-media.com/astonmartin/v12-vanquish/2002/oem/2002_astonmartin_v12-vanquish_coupe_base_s_oem_1_500.jpg", description: "James Bond.")
+# product.save
